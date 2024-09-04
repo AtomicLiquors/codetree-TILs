96e039d0ba608d38b3264824777966472b546f0e
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2024-09-04 20:55:41 +0900

    Title: 물건 구입 비용, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240904/물건 구입 비용/cost-of-purchase.js
+++ 240904/물건 구입 비용/cost-of-purchase.js
@@ -4,19 +4,25 @@ const lines = fs.readFileSync(0, 'utf-8').split('\n');
 
 const [N, _] = lines[0].split(' ').filter(s => s !== '').map(Number);
 
-const DP = new Array(2 * N + 1).fill(Number.MAX_SAFE_INTEGER);
+let maxAmount = N;
+
+const shops = lines.slice(1).map((line) => {
+    const [amount, price] = line.split(' ').filter(s => s !== '').map(Number);
+    maxAmount = Math.max(amount, maxAmount);
+    return [amount, price];
+})
+
+const DP = new Array(maxAmount+1).fill(Number.MAX_SAFE_INTEGER);
 DP[0] = 0;
 
-lines.slice(1).forEach((line) => {
-    const [amount, price] = line.split(' ').filter(s => s !== '').map(Number);
-    
-    for(let n = amount; n <= 2*N; n++){
-        if(DP[n - amount] === Number.MAX_SAFE_INTEGER)
-            continue;
-        DP[n] = Math.min(price + DP[n - amount], DP[n]);
+shops.forEach((shop) => {
+    const [amount, price] = shop;
+    for(let n = amount; n <= maxAmount; n++){
+        if(DP[n - amount] !== Number.MAX_SAFE_INTEGER)
+            DP[n] = Math.min(price + DP[n - amount], DP[n]);
     }
 })
 
-let answer = Math.min(...DP.slice(N, 2 * N));
+let answer = Math.min(...DP.slice(N, maxAmount+1));
 
 console.log(answer);
