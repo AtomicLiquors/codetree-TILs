400e314558db1b44b017e68171c28914a3e9e619
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2024-09-04 20:34:22 +0900

    Title: 물건 구입 비용, Time: 165ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240904/물건 구입 비용/cost-of-purchase.js
+++ 240904/물건 구입 비용/cost-of-purchase.js
@@ -2,19 +2,21 @@ const fs = require('fs');
 const lines = fs.readFileSync(0, 'utf-8').split('\n');
 
 
-const [N, P] = lines[0].split(' ').filter(s => s !== '').map(Number);
+const [N, _] = lines[0].split(' ').filter(s => s !== '').map(Number);
 
-const DP = new Array(N+1).fill(Number.MAX_SAFE_INTEGER);
+const DP = new Array(2 * N + 1).fill(Number.MAX_SAFE_INTEGER);
 DP[0] = 0;
 
 lines.slice(1).forEach((line) => {
     const [amount, price] = line.split(' ').filter(s => s !== '').map(Number);
     
-    for(let n = amount; n <= N; n++){
+    for(let n = amount; n <= 2*N; n++){
         if(DP[n - amount] === Number.MAX_SAFE_INTEGER)
             continue;
         DP[n] = Math.min(price + DP[n - amount], DP[n]);
     }
 })
 
-console.log(DP[N]);
+let answer = Math.min(...DP.slice(N, 2 * N));
+
+console.log(answer);
